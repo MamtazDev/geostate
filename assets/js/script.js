fe60c2d6d1d5d4dc0fb23b94cd60db52f616e2b5
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-02-22 18:31:11 +0600

    ::update

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,46 @@ $(window).on("load", function () {
 $(document).ready(function () {
   /* code execute from here */
 });
+
+// faq_accordion_wrapper
+function toggleAccordion(element) {
+  element.classList.toggle("active");
+}
+const accordionTitles = document.querySelectorAll(".accordionTitle");
+accordionTitles.forEach((accordionTitle) => {
+  accordionTitle.addEventListener("click", () => {
+    const isActive = accordionTitle.classList.contains("is-open");
+    // Remove is-open class from all accordion items
+    accordionTitles.forEach((title) => {
+      title.classList.remove("is-open");
+    });
+    // Add is-open class to the clicked accordion item
+    if (!isActive) {
+      accordionTitle.classList.add("is-open");
+    }
+  });
+});
+// accordion end
+
+// Fancy slider
+const container = document.getElementById("myCarousel");
+const options = {
+  infinite: true,
+  Navigation: false,
+  center: false,
+  slidesPerPage: "auto",
+  transition: false,
+  // Dots: false,
+};
+
+new Carousel(container, options);
+// fancy slider end
+
+// splide slider
+document.addEventListener("DOMContentLoaded", function () {
+  var elms = document.getElementsByClassName("splide");
+  for (var i = 0; i < elms.length; i++) {
+    new Splide(elms[i]).mount();
+  }
+});
+// splide slider end
